9ee134f663259844ca6a06f332e0752bbda3e021
Yulian Planas <69726138+Jeyzen@users.noreply.github.com>
2024-02-29 17:35:01 -0300

    Exercise #2

---
+++ HackerRank/MatLogo.py
@@ -0,0 +1,73 @@
+# Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+# Mat size must be X. ( is an odd natural number, and  is  times .)
+# The design should have 'WELCOME' written in the center.
+# The design pattern should only use |, . and - characters.
+# Sample Designs
+
+#     Size: 7 x 21 
+#     ---------.|.---------
+#     ------.|..|..|.------
+#     ---.|..|..|..|..|.---
+#     -------WELCOME-------
+#     ---.|..|..|..|..|.---
+#     ------.|..|..|.------
+#     ---------.|.---------
+    
+#     Size: 11 x 33
+#     ---------------.|.---------------
+#     ------------.|..|..|.------------
+#     ---------.|..|..|..|..|.---------
+#     ------.|..|..|..|..|..|..|.------
+#     ---.|..|..|..|..|..|..|..|..|.---
+#     -------------WELCOME-------------
+#     ---.|..|..|..|..|..|..|..|..|.---
+#     ------.|..|..|..|..|..|..|.------
+#     ---------.|..|..|..|..|.---------
+#     ------------.|..|..|.------------
+#     ---------------.|.---------------
+# Input Format
+
+# A single line containing the space separated values of  and .
+
+# Constraints
+
+# Output Format
+
+# Output the design pattern.
+
+# Sample Input
+
+# 9 27
+# Sample Output
+
+# ------------.|.------------
+# ---------.|..|..|.---------
+# ------.|..|..|..|..|.------
+# ---.|..|..|..|..|..|..|.---
+# ----------WELCOME----------
+# ---.|..|..|..|..|..|..|.---
+# ------.|..|..|..|..|.------
+# ---------.|..|..|.---------
+# ------------.|.------------
+
+def create_door_mat(rows, cols):
+    height = rows // 2
+
+    for i in range(height):
+        pattern = '.|.' * (2 * i + 1)
+        print(pattern.center(cols, '-'))
+
+    print('WELCOME'.center(cols, '-'))
+
+    for i in range(height - 1, -1, -1):
+        pattern = '.|.' * (2 * i + 1)
+        print(pattern.center(cols, '-'))
+        
+
+value = input().split()
+
+rows = int(value[0])
+cols = int(value[1])
+
+create_door_mat(rows, cols)
